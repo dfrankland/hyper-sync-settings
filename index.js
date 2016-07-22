ffc4eb62dca24b8c695c6d714bfeff4ea0d0fe06
Dylan Frankland <github@frankland.io>
2016-07-21 17:06:00 -0700

    Add click event to notifications

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const getGitConfig = require('./lib/getGitConfig');
 const restore = require('./lib/restore');
 const backup = require('./lib/backup');
 
-
 const title = 'hyperterm-sync-settings';
 exports.title = title;
 
@@ -15,6 +14,11 @@ exports.onWindow = getNotify(notifier => {
   if (notifier) notify = notifier;
 });
 
+const openSetup = `event => {
+  const { shell } = require('electron');
+  shell.openExternal('https://github.com/dfrankland/hyperterm-sync-settings#setup');
+}`;
+
 let config;
 const checkForMissingSettings = () => {
   config = getGitConfig();
@@ -24,14 +28,26 @@ const checkForMissingSettings = () => {
     return true;
   } else {
     if (!personalAccessToken && !gistId) {
-      notify(errorTitle, 'Settings not found! Click for more info.');
+      notify(
+        errorTitle,
+        'Settings not found! Click for more info.',
+        openSetup
+      );
     } else {
       if (!personalAccessToken) {
-        notify(errorTitle, '`personalAccessToken` not set! Click for more info.');
+        notify(
+          errorTitle,
+          '`personalAccessToken` not set! Click for more info.',
+          openSetup
+        );
       }
 
       if (!gistId) {
-        notify(errorTitle, '`gistId` not set! Click for more info.');
+        notify(
+          errorTitle,
+          '`gistId` not set! Click for more info.',
+          openSetup
+        );
       }
     }
 
